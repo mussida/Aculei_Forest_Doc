a0c554339e542605e2aae142da781ec8a1a330f2
mussida <96059227+mussida@users.noreply.github.com>
2025-02-04 23:35:28 +0100

    fix: snippet part

--- src/app/components/vertex-fragment/vertex-fragment.component.ts
+++ src/app/components/vertex-fragment/vertex-fragment.component.ts
@@ -9,23 +9,22 @@ import { Component } from '@angular/core';
 export class VertexFragmentComponent {
   canvasCode = `
   const canvas = document.querySelector("#canvas");
-    if (!canvas) {
-        console.warn("Canvas non trovato, WebGL non inizializzato.");
-        return;
-    }
+  if (!canvas) {
+      console.warn("Canvas non trovato, WebGL non inizializzato.");
+      return;
+  }
 
-    const gl = canvas.getContext("webgl");
+  const gl = canvas.getContext("webgl");
 
-    if (!gl) {
-        console.error("Errore: WebGL non supportato!");
-        return;
-    }
+  if (!gl) {
+      console.error("Errore: WebGL non supportato!");
+      return;
+  }
 `;
 
   shaderVariables = `
     webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
     webglUtils.setUniforms(meshProgramInfo, {}, material);
     webglUtils.drawBufferInfo(gl, bufferInfo);
-}
     `;
 }
